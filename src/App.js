af1729e5810eec8ec605ba5805b7febafc1cdfc7
Ahmet <115113818+Y1lmaz-Ahmet@users.noreply.github.com>
2023-02-06 18:06:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="squares">
+          <div className="square"></div>
+          <div className="square-inset"></div>
+        </div>
+        <div className="circles">
+          <div className="circle"></div>
+        </div>
+
+        <div className="test">
+          <div className="square-test"></div>
+        </div>
+        <div className="test-red">
+          <div className="test-red-square"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
